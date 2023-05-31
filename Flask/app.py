e2cb5d42930ce00a87fe0e69694cf07d388744e4
pakhi <pakhivashis182001@gmail.com>
2023-05-31 11:06:23 +0530

    rendering template in flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+    #return 'Hello, World!'
 
 
-if __name__ == "__main__":  ###to run program in debug mode
-    app.run(debug=True)
+if __name__ == "__main__":  #to run program in debug mode
+    app.run(debug=True,port=8000)
